ca6d9f9aac410ae8d9206ec0dbb408653eeb6916
rcsbnu27 <73672643+rcsbnu27@users.noreply.github.com>
2020-11-03 12:29:28 -0300

    Aula71
    Esta com erro no arquivo index.php na linha 34
    Fatal error: Uncaught Error: Class 'Sql' not found in C:\xampp\htdocs\Secao7aula71CriandoRESTAPIcomRotas\index.php:34 Stack trace: #0 [internal function]: {closure}() #1 C:\xampp\htdocs\Secao7aula71CriandoRESTAPIcomRotas\inc\Slim-2.x\Slim\Route.php(468): call_user_func_array(Object(Closure), Array) #2 C:\xampp\htdocs\Secao7aula71CriandoRESTAPIcomRotas\inc\Slim-2.x\Slim\Slim.php(1355): Slim\Route->dispatch() #3 C:\xampp\htdocs\Secao7aula71CriandoRESTAPIcomRotas\inc\Slim-2.x\Slim\Middleware\Flash.php(85): Slim\Slim->call() #4 C:\xampp\htdocs\Secao7aula71CriandoRESTAPIcomRotas\inc\Slim-2.x\Slim\Middleware\MethodOverride.php(92): Slim\Middleware\Flash->call() #5 C:\xampp\htdocs\Secao7aula71CriandoRESTAPIcomRotas\inc\Slim-2.x\Slim\Middleware\PrettyExceptions.php(67): Slim\Middleware\MethodOverride->call() #6 C:\xampp\htdocs\Secao7aula71CriandoRESTAPIcomRotas\inc\Slim-2.x\Slim\Slim.php(1300): Slim\Middleware\PrettyExceptions->call() #7 C:\xampp\htdocs\Secao7aula71CriandoRESTAPIcomRotas\index.php(46): Slim\Slim->run() #8 {main} in C:\xampp\htdocs\Secao7aula71CriandoRESTAPIcomRotas\index.php on line 34

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+require 'inc/Slim-2.x/Slim/Slim.php';
+
+\Slim\Slim::registerAutoloader();
+
+
+$app = new \Slim\Slim();
+
+
+$app->get(
+    '/',
+    function () {
+        require_once("view/index.php");
+    }
+);
+
+$app->get(
+    '/videos',
+    function () {
+        require_once("view/videos.php");
+    }
+);
+
+$app->get(
+    '/shop',
+    function () {
+        require_once("view/shop.php");
+    }
+);
+
+$app->get('/produtos', function(){
+
+    $sql = new Sql();
+
+    
+    $data = $sql->select("SELECT * FROM tb_produtos where preco_promorcional > 0 order by preco_promorcional desc limit 3;");
+    
+    var_dump($data);
+    exit;
+
+    echo json_encode($data);
+
+});
+
+$app->run();
+?>
